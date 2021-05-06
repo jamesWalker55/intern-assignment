7de65cabac98f2c7a96887ede509aeda1dd19f05
James Walker <james.chunho+github@gmail.com>
2021-05-06 18:34:49 +0100

    add comments to app.js

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const waitlistRouter = require("./routes/waitlist");
 const db = require("./db");
 const app = express();
 
+// middle stacks for express.js
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/", waitlistRouter);
+
+// custom error handler
 app.use(errorHandler)
 
 function errorHandler(err, req, res, next) {
